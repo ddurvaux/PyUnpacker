9dfa377376387f1cffb3ab9445ed492d05324e17
David Durvaux <david@autopsit.org>
2016-02-19 22:06:43 +0100

    Basic check implemented

--- test.py
+++ test.py
@@ -1,23 +1,26 @@
-
 #
 # !! TEST FILE for raw pieces of code !!
 #
-
 __author__ = 'David DURVAUX'
 __contact__ = 'david@autopsit.org'
 
+# IMPORTS
 import pefile
+
+
+# SETTINGS
 #binfile = "./demo/36a209a7d15d5d719d6072f45e4e3b46"
 binfile = "./demo/upx.exe"
+
 
 # load binary
 pe = pefile.PE(binfile)
 
-
-
 # check section + boundary and see if it matches
 page_size = 0x1000
 margin=0.1
+entropy_threshold = 7.0
+packed_score = 0
 
 SFLAGS = {
 	"CODE" : 0x00000020,
@@ -32,18 +35,24 @@ for section in pe.sections:
 	[name, vaddr, vsize, rsize, flags] = [section.Name, section.VirtualAddress, section.Misc_VirtualSize, section.SizeOfRawData, section.Characteristics]
 	
 	# check flags
-	if( flags ^ (SFLAGS["EXEC"] | SFLAGS["WRIT"]) ): # check if section is executable + writeable
+	if( int(flags ^ (SFLAGS["EXEC"] | SFLAGS["WRIT"])) == 0 ): # check if section is executable + writeable
 		print "ABNOMALIE SECTION SHOULD NOT BE WRITEABLE AND EXECUTABLE (W^X violation)!!"
-
+		packed_score += 1
 
 	# check sections sizes (incl. page alignment)
 	# the rsize need to be written in a multiple of memory page size (min 1.)
 	# a margin is added (could be customized)
 	if (rsize / page_size + 1) * page_size * (1 + margin) < vsize:
 		print "ABNOMALIES with VIRTUAL SIZE ALLOCATION for SECTION: %s" % name  
+		packed_score += 1
 
 	# check entropy
+	if(section.get_entropy() >= entropy_threshold):
+		print "ABNORMAL ENTROPY (%s)) for SECTION: %s" % (section.get_entropy(), name)	
+		packed_score += 1
 
-	#print("%s %s %s %s %s" % (name, hex(vaddr), vsize, rsize, hex(flags)))
+print "TOTAL PACKED SCORE: %s" % packed_score
 
-	# add score
+
+
+
