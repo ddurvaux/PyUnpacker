a0ec2d7a528936f36e11e5bd8c5a17bdcb81fc48
David DURVAUX <david@autopsit.org>
2016-06-22 17:41:48 +0200

    Draft IDA pluin in Python - will extract data to assist

--- static/ida.py
+++ static/ida.py
@@ -1,2 +1,26 @@
 # This class will implement IDA support for some of his strenghts
 # not yet done!! :)
+#
+# This is a part of the PyUnpacker package that works in IDA as a plugin
+#
+from idautils import *
+from idc import *
+
+def getPerFunctionHash():
+	functions = Functions()
+	for function in functions:
+		print "sub_%08x" % function # DEBUG
+
+def main():
+	print "IDA PYTHON PLUGIN STARTED!!"
+	
+	# TEST
+	getPerFunctionHash()
+
+
+if __name__ == "__main__":
+    main()
+
+# --------------------------------------------------------------------------- #
+# That's all folk ;)
+# --------------------------------------------------------------------------- #
