3fbee7e1b35ecdd635e3b4d7f3ccfae4ce587dcf
David DURVAUX <david@autopsit.org>
2016-06-22 17:52:13 +0200

    sync

--- static/ida.py
+++ static/ida.py
@@ -9,7 +9,14 @@ from idc import *
 def getPerFunctionHash():
 	functions = Functions()
 	for function in functions:
+		funcCode = getFunctionCode(function)
 		print "sub_%08x" % function # DEBUG
+
+def getFunctionCode(funcAddress):
+	chunks = Chunks(funcAddress)
+	for [start, end] in chunks:
+		print ("CHUNK: %s -> %s" % (start, end))
+	return None #NOT IMPLEMENTED!!
 
 def main():
 	print "IDA PYTHON PLUGIN STARTED!!"
